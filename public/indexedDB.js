b400ec680903cd0f7e5aed3b8bd1c2550b8d4a34
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2020-12-05 14:41:31 -0500

    init

---
+++ public/indexedDB.js
@@ -0,0 +1,15 @@
+
+    // create budget db + bulk (pending) collectiopn
+
+    // activate -> 
+
+    // saveRecord -> save to indexedDB
+
+    // listen online and send records
+
+    //schema
+    //if online check DB
+    // deal with it
+    //save record to db if offline
+    // back online, send to mongo and clear pending (indexedDB)
+    //listen for appcoming backonline
