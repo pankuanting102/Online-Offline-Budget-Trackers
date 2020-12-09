ba8487b6d231ce4e8906348c32f5e174ba1c9f94
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2020-12-08 22:50:09 -0500

    add indexDB and service-worker

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -10,28 +10,56 @@
     //save record to db if offline
     // back online, send to mongo and clear pending (indexedDB)
     //listen for appcoming backonline
+let db;
+const request = indexedDB.open("budget", 1);
 
-    request.onupgradeneeded = () => {
 
+    request.onupgradeneeded = ({target}) => {
+        let db = target.result;
+        db.createObjectStore("pending", {autoIncrement: true});
     }
 
 
-    request.onsuccess = () => {
-        
+    request.onsuccess = ({target}) => {
+        db = target.result;
+        if (navigator.onLine) {
+            checkDatabase ();
+        }
     }
 
 
-    request.onerror = () => {
-        
+
+
+    function saveRecord (data) {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.add(data)
     }
 
+    function checkDatabase () {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        const getAll = store.getAll();
 
-    request.saveRecord = () => {
-        
-    }
-
-    request.checkDatabase = () => {
-        
+        getAll.onsuccess = function () {
+            if (getAll.result.length > 0) {
+                fetch("/api/transaction/bulk", {
+                    method: "POST",
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: "application/json, text/palin, */*", "Content-Type": "applicatio/json"
+                    }
+                })
+                .then (response => {
+                    return response.json();
+                })
+                .then (() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store =transaction.objectStore("pending");
+                    store.clear();
+                })
+            }
+        }
     }
 
     window.addEventListener("online", checkDatabase)
